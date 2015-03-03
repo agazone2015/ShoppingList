a7030ce904e6a43f2e42cde380305aeb8490214a
agazone2015 <iGoosia@iGoosia-2.local>
2015-03-03 19:23:54 +1100

    Initial Commit

---
+++ ToDoList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToDoList
+//
+//  Created by Agnieszka Carroll on 3/03/2015.
+//  Copyright (c) 2015 Agnieszka Carroll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ToDoList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Agnieszka Carroll on 3/03/2015.
+//  Copyright (c) 2015 Agnieszka Carroll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
