eeb860b5fc907ad5a35f471e37093f5f5e4c8029
agazone2015 <agnescarroll@iGoosia-1.local>
2015-03-06 23:12:54 +1100

    To-Do List with list of items

--- ToDoList/ToDoListTests/ToDoListTests.m
+++ ToDoList/ToDoListTests/ToDoListTests.m
@@ -2,8 +2,8 @@
 //  ToDoListTests.m
 //  ToDoListTests
 //
-//  Created by Agnieszka Carroll on 3/03/2015.
-//  Copyright (c) 2015 Agnieszka Carroll. All rights reserved.
+//  Created by Agnes Carroll on 6/03/2015.
+//  Copyright (c) 2015 agazone. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
